7239f55b9ccb747bf30bf7345c82669b0c2d38fd
KENNS9 <nikenastri.393@gmail.com>
2025-05-27 08:20:05 +0700

    avatar list for profile section

---
+++ src/assets/avatarList.js
@@ -0,0 +1,15 @@
+const avatarList = [
+  "/avatars/dino-ava.jpg",
+  "/avatars/fox-ava.jpg",
+  "/avatars/ghost-ava.jpg",
+  "/avatars/kucing-ava.jpg",
+  "/avatars/kucing2-ava.jpg",
+  "/avatars/kucing3-ava.jpg",
+  "/avatars/monster-ava.jpg",
+  "/avatars/panda-ava.jpg",
+  "/avatars/paus-ava.jpg",
+  "/avatars/tupai-ava.jpg",
+  "/avatars/turtle-ava.jpg",
+];
+
+export default avatarList;
